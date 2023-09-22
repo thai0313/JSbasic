28628e9377bcd6ff50b3a41163096239c7fd8e11
thai0313 <ledangquoc313@gmail.com>
2023-09-22 19:26:48 +0700

    22/9/2023

--- lab01.js
+++ lab01.js
@@ -1,1 +1,63 @@
-console.log ("ád")
+// console.log("ádasd")
+//bài 1 
+//câu a
+// console.log("Hello world")
+// //câu b
+// console.log("Lê Đặng Quốc Thái")
+// //câu c
+// console.clear()
+// //câu d
+// console.log("*******")
+// console.log("*******")
+// console.log("*******")
+// console.log("*******")
+// console.log("*******")
+// //câu e
+// console.log("   *   ")
+// console.log("  *** ")
+// console.log(" ***** ")
+// console.log("*******")
+// //câu f
+// console.log("  **   ** ")
+// console.log("*    *    *")
+// console.log("*         *")
+// console.log(" *       *")
+// console.log("  *     *")
+// console.log("   *   *")
+// console.log("     * ")
+// //bài 2 
+// //câu a
+// let a=10
+// console.log(a)
+// //câu b
+// let b='10';
+// console.log(b)
+//câu c
+// a=20
+// console.log(a)
+// //câu d
+// const pi=3.14
+// console.log(pi)
+//câu e
+// pi=5
+// console.log(pi)
+//câu f
+let hocSinh={
+    hoten: 'le dang quoc thai',
+    tuoi: 18,
+    diachi: 'tren canh cay duoi ngon doi'
+}
+hocSinh.soDienThoai = 9128378123
+hocSinh.diachi = 'trai dat'
+// console.log(hocSinh)
+let dienthoai={
+    ten: 'iphone14',
+    hangSanXuat:'Apple',
+    mauSac: 'Trang',
+    gia:'1000USD'
+}
+console.log(dienthoai)
+dienthoai.gia = '750USD'
+console.log(dienthoai)
+dienthoai.ten = 'iPhone14 pro max'
+console.log(dienthoai)
