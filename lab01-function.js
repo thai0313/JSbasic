e55e1f7e34ddba715bd70eeaa5944dcc17544b51
thai0313 <ledangquoc313@gmail.com>
2023-09-25 19:39:35 +0700

    F

---
+++ lab01-function.js
@@ -0,0 +1,12 @@
+function tinhtong(a,b){
+    let ketqua1=a+b
+    return ketqua1
+}
+let kq1=tinhtong(2,4)
+console.log(kq1)
+function tinhhieu(a,b){
+    let ketqua2=a-b
+    return ketqua2
+}
+let kq2=tinhhieu(2,3)
+console.log(kq2)
--- lab01.js
+++ lab01.js
@@ -61,3 +61,64 @@
 // console.log(dienthoai)
 // dienthoai.ten = 'iPhone14 pro max'
 // console.log(dienthoai)
+// == so sánh giá trị 
+// === so sánh giá trị và kiểu dữ liệu
+// cau2 a
+// let a=5
+// let b=7
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// // câu b
+// console.log((a+b)/2)
+//câu c 
+// let x=3
+// let y=4
+// let z=6
+// console.log(x*x+2*x+1)
+// console.log(x*x*x-3*x*y-5*y+3*y*y)
+// console.log((x*y)*(x*y)-2*x*x*y+13*y)
+// console.log(4*x*x*x+3*x*y+y*y-(2*x*x-3*y))
+// console.log(((5*x*x)/(4*x*y))+y*y)
+// console.log(x*x-(2*x*y*z)/(y*y)+5*x-2*y*y+4*x*(z*z*z)+z*z*z)
+//câu d
+// let d=5
+// let chuvi=d/2*3.14
+// let dientich=(d*d)/4*3.14
+// console.log(chuvi)
+// console.log(dientich)
+//câu e
+// let dai=15
+// let rong=10
+// console.log((dai+rong)*2)
+// console.log(rong*dai)
+//câu f
+let quangdg=120
+let tgp=130
+let tgh=tgp/60
+let vantoc=quangdg/tgh
+console.log(vantoc)
+//câu g 
+let tgp2=150
+let tgh2=tgp/60
+let vantoc2=60
+let quangdg2=vantoc*tgh2
+console.log(vantoc2)
+//câu h
+let vantoc3=15.2
+let tgh3=3
+let quangdg3=vantoc3*tgh3
+console.log(vantoc3)
+//câu i
+let tg4=220
+let vantoc4=42
+let quangdg4=vantoc4*tg4
+console.log(vantoc4)
+//câu j
+let cao=6
+let dai=8
+let rong=5
+let thetich=6*5*8
+let khoigo=thetich*4
+console.log(khoigo)
