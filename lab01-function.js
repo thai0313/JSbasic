90a62c7d518d5693296e12463a602f6a0fb09587
thai0313 <ledangquoc313@gmail.com>
2023-09-27 09:53:21 +0700

    function

--- lab01-function.js
+++ lab01-function.js
@@ -1,12 +1,71 @@
+//bài 1
+//câu a
 function tinhtong(a,b){
-    let ketqua1=a+b
-    return ketqua1
+    let tong=a+b
+    return tong
 }
 let kq1=tinhtong(2,4)
 console.log(kq1)
+
 function tinhhieu(a,b){
-    let ketqua2=a-b
-    return ketqua2
+    let hieu=a-b
+    return hieu
 }
 let kq2=tinhhieu(2,3)
 console.log(kq2)
+
+function tinhtich(a,b){
+    let tich=a*b
+    return tich
+}
+let kq3=tinhtich(5,6)
+console.log(kq3)
+
+function tinhthuong(a,b){
+    let thuong =a/b
+    return thuong
+}
+let kq4=tinhthuong(9,3)
+console.log(kq4)
+
+//câu b
+function tbc(a,b){
+    let tbc=(a+b)/2
+    return tbc
+}
+let kq5=tbc(8,6)
+console.log(kq5)
+//câu d
+function chuvi(a){
+    let chuvi=a/2*3.14
+    return chuvi
+}
+let kqd1=chuvi(5)
+console.log(kqd1)
+function dientich(a){
+    let dientich=(a*a)/4*3.14
+    return dientich
+}
+let kqd2=dientich(5)
+console.log(kqd2)
+//câu e
+function chuvicn(a,b){
+    let chuvicn=(a+b)*2
+    return chuvicn
+}
+let kqe1=chuvicn(5,6)
+console.log(kqe1)
+function dientichcn(a,b){
+    let dientichcn=(a*b)
+    return dientichcn
+}
+let kqe2=dientichcn(3,4)
+console.log(kqe2)
+//câu f
+function vantoc(qd,tg){
+    let tgh=tg/60
+    let vantoc=qd/tgh
+    return vantoc
+}
+let kqf=vantoc(120,130)
+console.log(kqf)
